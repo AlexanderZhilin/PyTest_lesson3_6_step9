9b5767ac6920fb444f4171cb4381f5c8401fd9d6
Александр Жилин <modelinterior@yandex.ru>
2022-09-26 18:07:40 +0300

    add try_except in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,16 +1,22 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
 import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
-    browser.implicitly_wait(10)
+def test_add_to_cart_button(browser):
+    browser.implicitly_wait(5)
     browser.get(link)
-    check_btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     # time.sleep(5)
-    assert check_btn, "add to cart button not found"
+    try:
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basketp")))
+        found = True
+    except:
+        found = False
+
+    assert found, "add to cart button not found"
 
 # if __name__ == "__main__":
 #     pytest.main(['-s', '-v', '--browser_name=firefox', '--language=en', 'test_parser.py'])
